f5ab3f5382f2d571fd154cf8ec07e51c40006466
luochao <luochao@joyy.com>
2022-02-08 16:32:03 +0800

    feat: finish collection exercise

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -35,7 +35,7 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Put new fruits if not already present. Note that you
+        basket.entry(fruit).or_insert(4);
         // are not allowed to put any type of fruit that's already
         // present!
     }
